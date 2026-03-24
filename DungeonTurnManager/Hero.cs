d568e830fbd0c482a101834fad0d96d5c5c39e76
Bryelle <bryelle.kimball@students.snow.edu>
2026-03-24 14:28:17 -0600

    Add project files.

---
+++ DungeonTurnManager/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonTurnManager
+{
+    public class Hero
+    {
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Health { get; set; }
+
+        public Hero() { }
+        public Hero(string name, string heroClass)
+        {
+            Name = name;
+            Class = heroClass;
+        }
+
+        public string TakeTurn()
+        {
+            int damage = new Random().Next(1,100);
+            string action = $"{Name} the {Class} attacked! {damage} damage.";
+            Console.WriteLine(action);
+            return action;
+        }
+    }
+}
---
+++ DungeonTurnManager/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DungeonTurnManager;
+
+class Program
+{
+
+    static Queue<Hero> heroQueue = new Queue<Hero>();
+    static Stack<string> actionHistory = new Stack<string>();
+
+    static void Main(string[] args)
+    {
+        Hero bob = new Hero("Bob", "Bard");
+        Hero lily = new Hero("Lily", "Alchemist");
+        Hero randalf = new Hero("Randalf", "Wizard");
+
+        heroQueue.Enqueue(bob);
+        heroQueue.Enqueue(lily);
+        heroQueue.Enqueue(randalf);
+
+        int rounds = 1;
+
+        Console.WriteLine("~~~~~~ BATTLE IS BEGUN ~~~~~~");
+
+        while (rounds <= 7)
+        {
+
+            Hero currentHero = heroQueue.Dequeue();
+            Console.WriteLine($"\n{currentHero.Name}'s turn.");
+
+            string log = currentHero.TakeTurn();
+            actionHistory.Push(log);
+
+            heroQueue.Enqueue(currentHero);
+
+            Console.WriteLine("\nPress ENTER to continue, or type 'undo' to undo last action:");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "undo")
+            {
+                if (actionHistory.Count > 0)
+                {
+                    string lastAction = actionHistory.Pop();
+                    Console.WriteLine($"Undoing: {lastAction}");
+                }
+                else
+                {
+                    Console.WriteLine("Nothing to undo :)");
+                }
+            }
+
+            rounds++;
+        }
+
+        Console.WriteLine("\n~~~~~~ BATTLE COMPLETE ~~~~~~");
+    }
+}
